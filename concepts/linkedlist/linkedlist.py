6ade23f1ed6d4fbb3514901054c789157f5ed7fb
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2025-02-01 18:33:48 +0800

    -

--- concepts/linkedlist/linkedlist.py
+++ concepts/linkedlist/linkedlist.py
@@ -6,6 +6,29 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+       
+    def display(self):
+        # get the first node (self.head)
+        current = self.head
+      
+          # while there is still a next node (assigned from the line current = current.next)
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+            
+        print("NONE")
+    
+        
+    def insert_at_front(self, data):
+        new_node = Node(data)
+        current_head = self.head
+        
+        if current_head:
+            new_node.next = current_head
+        
+        self.head = new_node        
+        return 1
         
 
     def insert(self, data, index):
@@ -31,20 +54,27 @@ class LinkedList:
         new_node.next = current.next
         current.next = new_node
         return 1
-       
-       
-       
-    def display(self):
-        # get the first node (self.head)
-        current = self.head
-      
-          # while there is still a next node (assigned from the line current = current.next)
-        while current:
-            print(current.data,end=" -> ")
-            current = current.next
-            
-        print("NONE")
     
+
+        
+    def insert_at_back(self, data):
+        new_node = Node(data)
+        current = self.head
+       
+        if not current:
+           self.head = new_node
+           return 1
+           
+        else:
+           while True:
+               if not current.next:
+                   current.next = new_node
+                   break
+                
+               current = current.next
+
+        return 1
+
 
     def findAt(self, index):
         # get first node
@@ -58,21 +88,38 @@ class LinkedList:
             current = current.next
 
             if not current:
+                print("Index out of range")
                 return None
 
             index-=1
 
         return current
     
-    def insert_at_front(self, data):
-        new_node = Node(data)
-        current_head = self.head
+    def remove_node_at(self, index):
+            
+        current = self.head
         
-        if current_head:
-            new_node.next = current_head
+        if index == 0:
+            self.head = current.next
+            return 1
         
-        self.head = new_node
-             
+        # iterate until the current node is 1 before the specified node index
+        while (index-1) > 0:
+            current = current.next
+            
+            if not current:
+                return 0
+        
+            index-=1
+        
+        node_to_del = current.next
+        current.next = node_to_del.next
+        
+        del node_to_del
+        return 1
+        
+        
+
         
        
 # Test the implementationW
@@ -101,6 +148,13 @@ if __name__ == "__main__":
             break
         
     print("[INFO] Current linked list:", end="\n")
+    linked_list.display()
+    print('\n')
+    
+    linked_list.remove_node_at(4)
+
+        
+    print("[AFTER] Current linked list:", end="\n")
     linked_list.display()
     print('\n')
     
